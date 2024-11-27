5109f008753569b29c1a9586ace1416cfc7fc623
FaithChepkemoi <faithhope017@gmail.com>
2024-11-27 04:52:30 +0300

    Arithmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+let num1= 31;
+let num2= 2;
+let multiply= num1 *num2 ;
+let random= 1;
+Math.random();
+let num3= 14;
+let num4= 10;
+let mod = num3 % num4;
+let num5=20;
+// Define a function to find the maximum number
+function findMax(num1, num2, num3, num4) {
+    return Math.max(num1, num2, num3, num4); // Use Math.max to find the maximum
+}
+
+// Example usage
+let max = findMax(10, 15, 20, 5); // Call the function with four numbers
+console.log(max); // Outputs: 20
+
+
+
